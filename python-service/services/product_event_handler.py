886baf61105cb508f0b6e01b55b0afe3d9a89f2d
Alex Ma <alexma6614@gmail.com>
2019-08-11 22:47:40 -0400

    added some help

--- python-service/services/product_event_handler.py
+++ python-service/services/product_event_handler.py
@@ -2,4 +2,26 @@ import pika
 import json
 
 def emit_product_order(name):
-  pass
+    # 'rabbitmq-server' is the network reference we have to the broker, 
+    # thanks to Docker Compose.
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq-server'))
+    channel    = connection.channel()
+
+    
+    
+
+    # This will create the exchange if it doesn't already exist.
+    channel.exchange_declare(exchange=exchange_name, exchange_type='', durable=True)
+    
+    
+
+    channel.basic_publish(exchange=exchange_name,
+                          routing_key=routing_key,
+                          body=None,
+                          # Delivery mode 2 makes the broker save the message to disk.
+                          properties=pika.BasicProperties(
+                            delivery_mode = 2,
+                        ))
+
+    print("%r sent to factory %r with data: %r" % (routing_key, exchange_name, None))
+    connection.close()
